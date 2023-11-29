c3862cc453a828b5815cc9e6b060fce50ad4bc35
Uthman Jinadu <uthmanjinadu@gmail.com>
2023-11-29 16:13:33 -0500

    Updated

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ try:
         st.warning("No data available. Please check the data source.")
     
     # Rerun the app every 10 seconds
-    time.sleep(20)
+    time.sleep(10)
     st.experimental_rerun()
 
 except Exception as e:
